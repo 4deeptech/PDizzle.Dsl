b4ed92072225c4ba5ffa37f2200c215c90e996f8
Darren Ford <darren.ford@4deeptech.com>
2014-12-03 20:34:39 -0600

    initial add after setting up gitignore
    initial add after setting up gitignore

---
+++ Dsl/Custom/CustomCode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourDeep.PDizzle
+{
+    public partial class Property
+    {
+        //List<NameType> _nameTypeListClassParams = new List<NameType>();
+        string _value = null;
+        public string GetTypeValue()
+        {
+            return _value;
+        }
+        public void SetTypeValue(string value)
+        {
+            if (value != null)
+                _value = value;
+        }
+    }
+}
